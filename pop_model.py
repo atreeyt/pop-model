e0397eef599189f872cf33266997e73caf726352
Benjamin <benjaminpnicholls@gmail.com>
2024-12-11 13:05:34 +0000

    add main file

---
+++ pop_model.py
@@ -0,0 +1,140 @@
+import argparse
+import logging
+import os
+from copy import deepcopy
+
+import population_model
+import utils
+
+
+def parse_arguments() -> argparse.ArgumentParser:
+    """Parses command line arguments."""
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-t', help='max number of timesteps', dest='maxtime', 
+                        type=int)
+    parser.add_argument('-l', '--log',
+                        help='Log to default file (logs/)',
+                        dest='use_logger', action='store_true')
+    parser.add_argument('--log_file',
+                        help='Log file name. Should end with ".log".'
+                        ' Default "basic.log".',
+                        dest='log_name', default=None)
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help="Increase verbosity level (use -v for verbose, -vv for more verbose)")
+    parser.add_argument('--level', 
+                        help='Set logging level'
+                            ' [debug, info, warning, error, critical]'
+                            ' (default warning), overrides -v', 
+                        dest='log_level', default=None) 
+    args = parser.parse_args()
+    return args
+
+def config(use_logger=False, log_name=None, log_folder=None, 
+           log_level='warning', verbose=0) -> None:
+    """Defines any configuration for the file."""
+
+    assert log_level in [
+        'debug', 'info', 'warning', 'error', 'critical', None
+    ], f'Unknown logging level: {log_level}.'
+
+    match log_level:
+        case 'debug': 
+            log_level = logging.DEBUG
+        case 'info': 
+            log_level = logging.INFO
+        case 'warning': 
+            log_level = logging.WARNING
+        case 'error': 
+            log_level = logging.ERROR
+        case 'critical': 
+            log_level = logging.CRITICAL
+        case _:
+            if args.verbose == 0:
+                log_level = logging.WARNING # default
+            elif args.verbose == 1: 
+                log_level = logging.INFO
+            elif args.verbose == 2:
+                log_level = logging.DEBUG
+            elif args.verbose > 2:
+                log_level = logging.DEBUG
+                logging.warning('Verbosity set >2 has no effect.')
+
+    if use_logger:
+        if log_folder is None:
+            log_folder = 'logs'
+        if log_name is None:
+            log_name = 'basic.log'
+        # Create path logging directory.
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.join(dir_path, log_folder, log_name)
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+ 
+        logging.basicConfig(
+            level=log_level,
+            format='%(asctime)s %(levelname)s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            filename=f'{path}'
+        )
+    else:
+        logging.basicConfig(
+            level=log_level,
+            format='%(asctime)s %(levelname)s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+        )
+    return
+
+def events(pop_model: population_model.PopulationModel,
+           t: int) -> population_model.PopulationModel:
+    """Define events that occur at time t. Returns modified population model.
+
+    At times t, certain events can happen to a population, such as culling
+    a susceptible population. These changes occur here and the resultant
+    population model is returned.
+    """
+    match t:
+        case 0:
+            pop_model.add_seeds('rr', 900)
+            pop_model.add_seeds('RR', 110)
+        case t if t > 1:
+            # Remove 80% of the 'rr' population.
+            # TODO make this variable.
+            count = pop_model.get_population()['rr']
+            pop_model.remove_seeds('rr', count*0.8)
+
+    return pop_model
+
+def main(max_time=1) -> None:
+    # List to store each iteration. Python passes the variable around as a
+    # reference, so the object stored in the list will be modified after it
+    # has been added.
+    iteration_history = []
+
+    # Compute for number of time steps t.
+    for t in range(0, max_time+1):
+        print(f'--- Time {t} ---')
+        if t == 0:
+            pop_model = population_model.PopulationModel()
+        else:
+            pop_model = deepcopy(iteration_history[t-1])
+        pop_model = events(pop_model, t)
+        iteration_history.append(pop_model)
+
+        # Model population changes after the start.
+        if t > 0:
+            results = pop_model.calculate_population_change()
+
+        # Showing results.
+        print('  pop:', utils.round_dict_values(pop_model.get_population()))
+        print('  freq:', utils.round_dict_values(pop_model.get_frequency()))
+
+    return
+
+if __name__ == "__main__":
+    args: argparse.ArgumentParser = parse_arguments()
+    config(use_logger=args.use_logger,
+           log_name=args.log_name,
+           log_level=args.log_level,
+           verbose=args.verbose)
+    logging.debug('----- BEGIN PROGRAM -----')
+    main(args.maxtime)
