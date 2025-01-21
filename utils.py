610e8d5f274fd7bde007ab6ef5caf0d52dc73d21
Benjamin <benjaminpnicholls@gmail.com>
2025-01-21 14:53:02 +0000

    fix empty dict values error

--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
 def round_dict_values(dictionary, n=3):
     """Round dictionary to `n` decimal places."""
     # TODO check if complex number can be converted to float.
-    for val in dictionary.values():
-        if not isinstance(val, (int, float, complex)):
-            raise ValueError("{0} is not numeric".format(val))
+    # for val in dictionary.values():
+    # TODO guard for numbers, but needs to allow an empty list
+    # TODO maybe also need None ^^
+    #     if not isinstance(val, (int, float, complex)):
+    #         print(dictionary)
+    #         raise ValueError("{0} is not numeric".format(val))
 
-    rounded = {key: float(f"{val:.{n}f}") for key, val in dictionary.items()}
+    # rounded = {key: float(f"{val:.{n}f}") for key, val in dictionary.items()}
+    rounded = {
+        key: round(value, 2) if isinstance(value, (int, float)) else value
+        for key, value in dictionary.items()
+    }
     return rounded
 
 
