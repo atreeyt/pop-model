80aeec95bcb50ada75bd5521db61bcae0a13b181
Benjamin <benjaminpnicholls@gmail.com>
2025-01-30 14:55:10 +0000

    fix verbosity. add/fix docstrings. add 'location' parameter. add calculation for year from t.

--- pop_main.py
+++ pop_main.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 from copy import deepcopy
+from math import ceil
 
 import observer_model
 import population_model
@@ -76,13 +77,13 @@ def config(
         case "critical":
             log_level = logging.CRITICAL
         case _:
-            if args.verbose == 0:
+            if verbose == 0:
                 log_level = logging.WARNING  # default
-            elif args.verbose == 1:
+            elif verbose == 1:
                 log_level = logging.INFO
-            elif args.verbose == 2:
+            elif verbose == 2:
                 log_level = logging.DEBUG
-            elif args.verbose > 2:
+            elif verbose > 2:
                 log_level = logging.DEBUG
                 logging.warning("Verbosity set >2 has no effect.")
 
@@ -113,7 +114,11 @@ def config(
 
 
 def get_resistant_seed_freq_from_pop(seed_dictionary, susceptible_list=["rr"]) -> float:
-    """TODO"""
+    """Get the frequency of the resistant seeds within the given population.
+
+    Returns a float [0-1].
+    """
+
     population = sum(seed_dictionary.values())
     total_count = 0
     for chromosome, count in seed_dictionary.items():
@@ -126,7 +131,12 @@ def get_resistant_seed_freq_from_pop(seed_dictionary, susceptible_list=["rr"]) -
 def get_resistant_seed_freq_from_freq(
     seed_freq_dictionary, susceptible_list=["rr"]
 ) -> float:
-    """TODO"""
+    """Given a dictionary of the frequency of each seed type within a
+    population, returns the frequency of resistant seeds.
+
+    Returns a float [0-1].
+    """
+
     total_count = 0
     for chromosome, count in seed_freq_dictionary.items():
         if chromosome in susceptible_list:
@@ -136,22 +146,40 @@ def get_resistant_seed_freq_from_freq(
 
 
 def get_population_history(
-    iteration_history: list[population_model.PopulationModel],
+    iteration_history: list[population_model.PopulationModel], location
 ) -> list:
-    """TODO"""
+    """Get the population history of each iteration of the model.
+
+    Returns a list containing absolute population values.
+    """
+
+    assert location in [
+        "underground",
+        "overground",
+    ], f"location must be ['underground','overground']"
+
     return [
-        sum(t.get_population(location="overground").values()) for t in iteration_history
+        sum(t.get_population(location=location).values()) for t in iteration_history
     ]
 
 
 def get_resistance_history(
-    iteration_history: list[population_model.PopulationModel],
+    iteration_history: list[population_model.PopulationModel], location
 ) -> list:
-    """TODO"""
-    return [
-        get_resistant_seed_freq_from_freq(t.get_frequency(location="overground"))
+    """Get the resistance history of each iteration of the model.
+
+    Returns a list containing resistance values.
+    """
+    resistance_history = [
+        get_resistant_seed_freq_from_freq(t.get_frequency(location=location))
         for t in iteration_history
     ]
+    return resistance_history
+
+
+def calculate_year_from_t(t, time_steps_per_year) -> int:
+    # math.ceil
+    return ceil(t / time_steps_per_year)
 
 
 def events(
@@ -159,25 +187,34 @@ def events(
 ) -> population_model.PopulationModel:
     """Define events that occur at time t. Returns modified population model.
 
-    At times t, certain events can happen to a population, such as culling a susceptible population. These changes occur here and the resultant population model is returned.
+    At times t, certain events can happen to a population, such as
+    culling a susceptible population. These changes occur here and the
+    resultant population model is returned.
+
+    year = math.ceil(t/(t per year))
     """
+    # TODO add TIME_STEPS_PER_YEAR
+    #   Calculate when to germinate, purge, and return seeds based on
+    #   this value.
     match t:
         case 0:
             pop_model.add_seeds("rr", 100, location="underground")
             pop_model.add_seeds("Rr", 1, location="underground")
-        # case t if t > 0:
-        #     # Remove 80% of the 'rr' population.
-        #     # TODO make this variable.
-        #     pop_model.purge_population(0.8)
+
+        # When 2t per year, all odd t are midyear.
+        case t if not utils.is_even(t):
+            pop_model.return_seeds_to_seedbank(rate=1.0)
+            pop_model.germinate_seeds(rate=0.5)
+
+        # When 2t per year, all even t are end of year.
         case t if utils.is_even(t):
             # An effective herbicide but only targeting susceptible individuals.
             pop_model.purge_population(0.8, ["rr"], location="overground")
-        case t if not utils.is_even(t):
-            # Herbicide with less efficacy but different mode of action, all seeds susceptible.
-            pop_model.purge_population(
-                0.4, ["rr", "rR", "Rr", "RR"], location="overground"
-            )
-
+            pop_model
+        # Herbicide with less efficacy but different mode of action, all seeds susceptible.
+        # pop_model.purge_population(
+        #     0.4, ["rr", "rR", "Rr", "RR"], location="overground"
+        # )
     return pop_model
 
 
@@ -185,11 +222,12 @@ def main(max_time=1) -> None:
     # List to store each iteration. Python passes the variable around as a
     # reference, so the object stored in the list will be modified after it
     # has been added.
-    iteration_history = []
+    TIME_STEPS_PER_YEAR = 2
+    iteration_history: list[population_model.PopulationModel] = []
 
     # Compute for number of time steps t.
     for t in range(0, max_time + 1):
-        print(f"--- Time {t} ---")
+        print(f"\n\n--- Time {t} ---")
         if t == 0:
             pop_model = population_model.PopulationModel()
         else:
@@ -201,7 +239,7 @@ def main(max_time=1) -> None:
 
         # Model population changes after the start.
         if t > 0:
-            results = pop_model.get_population_change()
+            results = pop_model.apply_population_change()
 
         # Showing results.
         population = pop_model.get_population(location="overground")
@@ -220,16 +258,16 @@ def main(max_time=1) -> None:
     # Print lists of population and resistance history for graphs.
     # TODO matplotlib
     print("\npopulation history:")
-    print(get_population_history(iteration_history))
+    print(get_population_history(iteration_history, location="overground"))
     print("\nfrequency history:")
-    print(get_resistance_history(iteration_history))
+    print(get_resistance_history(iteration_history, location="overground"))
 
     print("\n\n NOISY OBSERVER\n----------------")
     observer = observer_model.ObserverModel(
         observation_accuracy=0.9, noise_standard_dev=0.05
     )
     for t, model in enumerate(iteration_history):
-        print(f"--- Time {t} ---")
+        print(f"\n\n--- Time {t} ---")
         # print("model.get_population():", model.get_population())
         population = observer.observe(model, noisy=True)
         print(
